e62aace1998cd60dc435f10e4dbfdce7b5646ea0
✧˖°.Xorginho✧˖° <144127740+xorginhoo@users.noreply.github.com>
2023-11-07 11:16:34 -0300

    Update and rename Questão14.c to questão14.c

--- questão14.c
+++ questão14.c
@@ -6,13 +6,13 @@ int main () {
 
     setlocale(LC_ALL, "portuguese");
 
-    // Variáveis
+    // VariÃ¡veis
     int numero, par = 0, impar = 0, quantidadegeral = 0, somageral = 0, somapar = 0, somaimpar = 0;
     float mediageral, mediaimpar, mediapar;
 
     //
     do {
-        printf("Digite o %d° número: ", quantidadegeral + 1);
+        printf("Digite o %dÂ° nÃºmero: ", quantidadegeral + 1);
         scanf("%d", &numero);
 
         if (numero > 0) {
@@ -35,11 +35,11 @@ int main () {
     mediaimpar = somaimpar / (float) impar;
 
     //
-    printf("\nQuantidade de números pares: %d \n", par);
-    printf("Quantidade de números ìmpares: %d \n", impar);
-    printf("Média dos números pares: %.1f \n", mediapar);
-    printf("Média dos números ìmpares: %.1f \n", mediaimpar);
-    printf("Média geral dos números: %.1f \n", mediageral);
+    printf("\nQuantidade de nÃºmeros pares: %d \n", par);
+    printf("Quantidade de nÃºmeros Ã¬mpares: %d \n", impar);
+    printf("MÃ©dia dos nÃºmeros pares: %.1f \n", mediapar);
+    printf("MÃ©dia dos nÃºmeros Ã¬mpares: %.1f \n", mediaimpar);
+    printf("MÃ©dia geral dos nÃºmeros: %.1f \n", mediageral);
 
     return 0;
 
